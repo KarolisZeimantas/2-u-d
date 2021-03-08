b18c3af8c20a0aa0a63b176a49406b36bed5c0dd
KarolisZeimantas <78847475+KarolisZeimantas@users.noreply.github.com>
2021-03-08 21:42:10 +0200

    1.3.1v

--- structures.h
+++ structures.h
@@ -1,4 +1,10 @@
+#ifndef GRANDPARENT_H1
+#define GRANDPARENT_H1
 #include<string>
+extern int studCount;
+extern std::string fill;
+extern int wantedMarks;
+extern char type;
 struct Students
 {
     std::string names;
@@ -7,3 +13,5 @@ struct Students
     float med=0;
     float egzaminas;
 };
+
+#endif /* GRANDPARENT_H1 */
--- usings.h
+++ usings.h
@@ -1,3 +1,5 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -8,6 +10,8 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <stdint.h>
+#include"structures.h"
+
 using std::cout;
 using std::cin;
 using std::string;
@@ -16,3 +20,14 @@ using std::setw;
 using std::move;
 using std::vector;
 using std::fixed;
+
+bool customer_sorter(Students const&, Students const&);
+void autoFill(vector<Students> &);
+void handFill( vector<Students> &);
+void fillFunc( vector<Students> &);
+void printTable( vector<Students> &);
+void scanFile(vector<Students> &);
+bool checkForDigit(string);
+
+
+#endif /* GRANDPARENT_H */
